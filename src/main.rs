88abb5ffbc9f6933bf39a49ce6229fd2757eef6e
FinleyConway <finleyconway03@gmail.com>
2025-02-05 22:12:15 +0000

    Rough impl of gathering and displaying music

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,54 @@
 mod window;
 mod audio;
 
+use std::env;
 use std::path;
 
-use audio::audio_reader::AudioReader;
+use audio::{audio_clip::AudioClip, audio_reader::AudioReader};
 use window::application::Application;
 use ratatui;
 
-fn main() -> std::io::Result<()>
-{   
-    // let args: Vec<String> = env::args().collect();
-    // let path = &args[1];
-
-
-    let path = path::Path::new("/home/finley/Music/");
+fn try_load_audio(working_dir: &str) -> std::io::Result<Vec<path::PathBuf>>
+{
+    let path = path::Path::new(working_dir);
     let mut files = Vec::new();
 
     AudioReader::default().find(path, &mut |e| files.push(e))?;
 
-    dbg!(files);
+    return Ok(files);
+}
 
-    return Ok(());
+fn gather_clips(working_dir: &str) -> std::io::Result<Vec<AudioClip>>
+{
+    let files = try_load_audio(working_dir)?;
+    let mut clips: Vec<AudioClip> = Vec::default();
+
+    clips.reserve(files.len());
+
+    for file in files
+    {
+        clips.push(AudioClip::new(&file.display().to_string()));
+    }
+
+    return Ok(clips);
+}
+
+fn main() -> std::io::Result<()>
+{   
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2
+    {
+        panic!("Stop"); // obv temp
+    }
+
+    let path = &args[1];
+
+    let clips = gather_clips(&path)?;
+
     // initialse terminal and application
     let mut terminal = ratatui::init();
-    let application = Application::new().run(&mut terminal);
+    let application = Application::new(clips).run(&mut terminal);
 
     ratatui::restore();
 
--- src/window/application.rs
+++ src/window/application.rs
@@ -4,6 +4,8 @@ use crate::audio::{
 
 use ratatui;
 use ratatui::widgets;
+use ratatui::text;
+use ratatui::symbols;
 
 use crossterm::event;
 
@@ -11,16 +13,21 @@ pub struct Application
 {
     pub audio_player: AudioPlayer,
     should_close: bool,
+
+    clips: Vec<AudioClip>,
+    state: widgets::ListState,
 }
 
 impl Application
 {
-    pub fn new() -> Application
+    pub fn new(clips: Vec<AudioClip>) -> Application
     {
         Application
         {
             audio_player: AudioPlayer::new(),
             should_close: false,
+            clips: clips,
+            state: widgets::ListState::default(),
         }
     }
 
@@ -41,12 +48,6 @@ impl Application
     pub fn close(&mut self) -> ()
     {
         self.should_close = true;
-    }
-
-    fn temp(&mut self) -> () // test clip playing
-    {
-        let clip = AudioClip::new("/home/finley/Music/Ruby the Hatchet - Tomorrow Never Comes.mp3");
-        self.audio_player.try_play(&clip).unwrap();
     }
 
     fn handle_events(&mut self) -> std::io::Result<()>
@@ -73,27 +74,53 @@ impl Application
         match key_event.code
         {
             event::KeyCode::Char('q') => self.close(),
-            event::KeyCode::Char(' ') => self.temp(),
             event::KeyCode::Char('p') => self.audio_player.toggle_pause(),
             event::KeyCode::Char('s') => self.audio_player.stop(),
+            event::KeyCode::Down      => self.state.select_next(),
+            event::KeyCode::Up        => self.state.select_previous(),
+            event::KeyCode::Enter     => self.handle_selected(),
             _ => {}
         }
     }
 
-    fn draw(&self, frame: &mut ratatui::Frame) -> ()
+    fn handle_selected(&mut self)
+    {
+        if let Some(i) = self.state.selected() 
+        {
+            let clip = &self.clips[i];
+
+            self.audio_player.try_play(&clip).unwrap();
+        }
+    }
+    
+    fn draw(&mut self, frame: &mut ratatui::Frame) -> ()
     {
         let data = self.audio_player.try_get_playing_data();
+        let data = match data {
+            Some(data) => format!("Playing: {} - {}", data.get_song(), data.get_artist()),
+            None => "No Song Playing".to_string(),
+        };
 
-        match data
+        let block = widgets::Block::new()
+            .title(text::Line::raw(data).centered())
+            .borders(widgets::Borders::TOP)
+            .border_set(symbols::border::EMPTY);
+
+        
+        let mut songs: Vec<&str> = Vec::default();
+        songs.reserve(self.clips.len());
+
+        for clip in self.clips.iter()
         {
-            Some(data) =>
-            {
-                let text = widgets::Paragraph::new(format!("Playing: {}, By: {}", data.get_song(), data.get_artist()));
-
-                frame.render_widget(text, frame.area());
-            },
-            None => {}
+            songs.push(clip.get_title());
         }
+
+        let list = widgets::List::new(songs)
+            .block(block)
+            .highlight_symbol(">")
+            .highlight_spacing(widgets::HighlightSpacing::Always);
+
+        widgets::StatefulWidget::render(list, frame.area(), &mut frame.buffer_mut(), &mut self.state);
     }
 }
 
@@ -107,7 +134,7 @@ mod tests
     #[test]
     fn test_key_event() -> io::Result<()> 
     {
-        let mut app = Application::new();
+        let mut app = Application::new(Vec::default());
         app.handle_key_events(event::KeyCode::Char('q').into());
         assert_eq!(app.should_close, true);
 
