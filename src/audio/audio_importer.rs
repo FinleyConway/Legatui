f090209580a0d945f81d30f5ef23d041adbd3bf7
FinleyConway <finleyconway03@gmail.com>
2025-02-06 11:53:44 +0000

    Convert audio reader into a dedicated importer

--- src/audio/audio_importer.rs
+++ src/audio/audio_importer.rs
@@ -1,12 +1,53 @@
 use std::{io, fs};
 use std::path;
+use std::env;
+
+use crate::AudioClip;
 
 #[derive(Default)]
-pub struct AudioReader;
+pub struct AudioImporter;
 
-impl AudioReader
+impl AudioImporter
 {
-    pub fn find(&self, path: &path::Path, func: &mut dyn FnMut(path::PathBuf) -> ()) -> io::Result<()>
+    pub fn gather_clips_args(&self) -> std::io::Result<Vec<AudioClip>>
+    {
+        let files = self.try_load_audio(&self.read_args())?;
+        let mut clips: Vec<AudioClip> = Vec::default();
+    
+        clips.reserve(files.len());
+    
+        for file in files
+        {
+            clips.push(AudioClip::new(&file.display().to_string()));
+        }
+    
+        return Ok(clips);
+    }
+
+    fn read_args(&self) -> String
+    {
+        let args: Vec<String> = env::args().collect();
+
+        // very simple, just checks if it has a passed in directory
+        if args.len() == 2
+        {
+            return args[1].clone();
+        }
+
+        return args[0].clone();
+    }   
+
+    fn try_load_audio(&self, working_dir: &str) -> std::io::Result<Vec<path::PathBuf>>
+    {
+        let path = path::Path::new(working_dir);
+        let mut files = Vec::new();
+    
+        self.find(path, &mut |e| files.push(e))?;
+    
+        return Ok(files);
+    }
+
+    fn find(&self, path: &path::Path, func: &mut dyn FnMut(path::PathBuf) -> ()) -> io::Result<()>
     {
         // recurse through a directory and callback a audio file found
         if path.is_dir()
--- src/main.rs
+++ src/main.rs
@@ -1,50 +1,13 @@
 mod window;
 mod audio;
 
-use std::env;
-use std::path;
-
-use audio::{audio_clip::AudioClip, audio_reader::AudioReader};
+use audio::{audio_clip::AudioClip, audio_importer::AudioImporter};
 use window::application::Application;
 use ratatui;
 
-fn try_load_audio(working_dir: &str) -> std::io::Result<Vec<path::PathBuf>>
-{
-    let path = path::Path::new(working_dir);
-    let mut files = Vec::new();
-
-    AudioReader::default().find(path, &mut |e| files.push(e))?;
-
-    return Ok(files);
-}
-
-fn gather_clips(working_dir: &str) -> std::io::Result<Vec<AudioClip>>
-{
-    let files = try_load_audio(working_dir)?;
-    let mut clips: Vec<AudioClip> = Vec::default();
-
-    clips.reserve(files.len());
-
-    for file in files
-    {
-        clips.push(AudioClip::new(&file.display().to_string()));
-    }
-
-    return Ok(clips);
-}
-
 fn main() -> std::io::Result<()>
 {   
-    let args: Vec<String> = env::args().collect();
-
-    if args.len() != 2
-    {
-        panic!("Stop"); // obv temp
-    }
-
-    let path = &args[1];
-
-    let clips = gather_clips(&path)?;
+    let clips = AudioImporter::default().gather_clips_args()?;
 
     // initialse terminal and application
     let mut terminal = ratatui::init();
